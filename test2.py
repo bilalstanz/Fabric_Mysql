18f5d035cff7871ba74df435d1e184c7de2c9120
Bilal Khan <bilal.khan@indiqus.com>
2022-02-13 08:40:28 +0530

    removed bunch of unnecessary test files

--- test2.py
+++
@@ -1,6 +0,0 @@
-
-import fabric
-
-from pprint import pprint
-c = fabric.connection.Connection('what')
-pprint(c.run())
--- test3.py
+++
@@ -1,5 +0,0 @@
-
-import logging
-
-logging.basicConfig(filename='mysql.log', filemode='w', level=logging.INFO)
-logging.info('updating package repo dhbfjb')
